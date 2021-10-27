bc54bdc1ac2054c4e34683ef3c3278aca8168388
Vitaopb <victor.lieuthier@gmail.com>
2021-10-27 16:22:44 -0300

    starter

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello {{ name }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    return {
+      name: "Victor"
+    }
+  }
+})
+</script>
+
